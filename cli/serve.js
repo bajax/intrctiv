b8f7dfb08f4aa7e829aaeda0302f254c28397362
Bajax <bajax@bajax.us>
2019-05-23 15:55:09 -0400

    now using nconf to manage conf
    just listen port for now.

--- cli/serve.js
+++ cli/serve.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const app     = express();
-const server  = app.listen(9900);
+const pJoin  = require('path').join;
+const config = require('nconf')
+	.argv()
+	.env()
+	.file('conf', pJoin(__dirname, '../config.json'))
+	.file('env', pJoin(__dirname, `../config.${process.NODE_ENV||'development'}.json`))
+	.get();
+
+const express  = require('express');
+const app      = express();
+if (!config.listen)
+	throw Error('no listen directive.  Is your conf good?');
+const server   = app.listen(config.listen);
 const Interact = require('../app');
+const debug    = require('debug')('intrctiv:serve');
 
 let interctv = Interact(server);
 app.use(interctv);
 
 server.on('listening', () => 
 {
-	console.log('now listening on port 9900');
+	console.log('now listening on ' + config.listen);
 });
