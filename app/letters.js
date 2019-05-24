dfb7787cc099dc86c58b2fd15cfb2d67f99d5d26
Bajax <bajax@bajax.us>
2019-05-23 20:12:35 -0400

    letters - massive updates
    mobile/touch support, letters now line up along the bottom at start, auto-resizes to keep the entire fridge on the screen.

--- app/letters.js
+++ app/letters.js
@@ -1,46 +1,70 @@
 'use strict';
 const debug = require('debug')('ia:app:letters');
 
-const SETS               = 8;
+const SYMBOLS = [
+                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+                 '!', '?', '.'
+                 ];
+
+const SETS               = 3;
 const SCRABBLE_DIST      = false;
 const HORIZONTAL         = true;
-const WIDTH              = 4;
-const HEIGHT             = 4;
 const GUTTER             = .5;
-const MARGIN             = 1;
-const MAXX               = 160-WIDTH;
-const MAXY               = 90-HEIGHT;
-const COLOR_CYCLE_FACTOR = .9;
+const MARGIN             = .5;
+const CWIDTH             = 30;
+const CHEIGHT            = 40;
+const PER_RANK           = Math.ceil(SYMBOLS.length/5 * SETS);
+const WIDTH              = (CWIDTH - MARGIN * 2 - (PER_RANK - 1) * GUTTER)/PER_RANK;
+const HEIGHT             = 2;
+const MAXX               = CWIDTH-WIDTH;
+const MAXY               = CHEIGHT-HEIGHT;
+const COLOR_CYCLE_FACTOR = .5;
 const MIN_INTENSITY      = .4;
 
-const LETTERS =
+const COUNTS =
 {
-	A : {count : 9},
-	B : {count : 2},
-	C : {count : 2},
-	D : {count : 4},
-	E : {count : 12},
-	F : {count : 2},
-	G : {count : 2},
-	H : {count : 2},
-	I : {count : 9},
-	J : {count : 1},
-	K : {count : 1},
-	L : {count : 4},
-	M : {count : 2},
-	N : {count : 6},
-	O : {count : 8},
-	P : {count : 2},
-	Q : {count : 1},
-	R : {count : 6},
-	S : {count : 4},
-	T : {count : 6},
-	U : {count : 4},
-	V : {count : 2},
-	W : {count : 2},
-	X : {count : 1},
-	Y : {count : 2},
-	Z : {count : 1},
+	A   :  9,
+	B   :  2,
+	C   :  2,
+	D   :  4,
+	E   : 12,
+	F   :  2,
+	G   :  2,
+	H   :  2,
+	I   :  9,
+	J   :  1,
+	K   :  1,
+	L   :  4,
+	M   :  2,
+	N   :  6,
+	O   :  8,
+	P   :  2,
+	Q   :  1,
+	R   :  6,
+	S   :  4,
+	T   :  6,
+	U   :  4,
+	V   :  2,
+	W   :  2,
+	X   :  1,
+	Y   :  2,
+	Z   :  2,
+	1   :  1,
+	2   :  1,
+	3   :  1,
+	4   :  1,
+	5   :  1,
+	6   :  1,
+	7   :  1,
+	8   :  1,
+	9   :  1,
+	0   :  1,
+	'?' :  1,
+	'.' :  1,
+	'!' :  1,
+	'!' :  1,
 };
 
 const MAGNETPROTO =
@@ -57,53 +81,56 @@ module.exports = (socket) =>
 	let magnets   = [];
 	let accum     = 0;
 	let moveAcc   = 0;
-	let rank      = 0;
 	let userCnt   = 0;
 	let maxUserID = 0;
 
-	for (let letterLBL in LETTERS)
+	for (let i = 0; i < SYMBOLS.length * SETS; i++)
 	{
-		let letter       = LETTERS[letterLBL];
-		letter.letter    = letterLBL;
+		let idx = i % SYMBOLS.length;
 
-		let destCnt = SCRABBLE_DIST ? letter.count : 1;
-		for (let cnt = 0; cnt < destCnt * SETS; cnt++)
+		let letterLBL = SYMBOLS[idx];
+
+		let destCnt = SCRABBLE_DIST ? COUNTS[letter] : 1;
+		for (let cnt = 0; cnt < destCnt; cnt++)
 		{
+			let rank = Math.floor(accum/PER_RANK);
+			let file = (accum%PER_RANK);
+
+			debug(letterLBL, rank, file);
+
 			let magnet    = Object.create(MAGNETPROTO);
 			magnet.letter = letterLBL;
 			let colorIDX;
 
-			if (SCRABBLE_DIST)
-				colorIDX = Object.keys(LETTERS).indexOf(letterLBL) * COLOR_CYCLE_FACTOR;
-			else
-				colorIDX = cnt / (destCnt * SETS) * Math.PI * 2 * COLOR_CYCLE_FACTOR;
+			colorIDX = accum / (destCnt * SETS) * Math.PI * 2 * COLOR_CYCLE_FACTOR;
 
 			magnet.color  =
 			[
-				Math.sin(colorIDX * COLOR_CYCLE_FACTOR              ) + MIN_INTENSITY,
-				Math.sin(colorIDX * COLOR_CYCLE_FACTOR + Math.PI/3*2) + MIN_INTENSITY,
-				Math.sin(colorIDX * COLOR_CYCLE_FACTOR + Math.PI/3*4) + MIN_INTENSITY
+				Math.sin(colorIDX * COLOR_CYCLE_FACTOR               ) + MIN_INTENSITY,
+				Math.sin(colorIDX * COLOR_CYCLE_FACTOR + Math.PI/3*2 ) + MIN_INTENSITY,
+				Math.sin(colorIDX * COLOR_CYCLE_FACTOR + Math.PI/3*4 ) + MIN_INTENSITY
 			];
 
-			magnet.id = ++accum;
+			magnet.id     = ++accum;
 			magnet.zIndex = magnet.id;
 
 			if (HORIZONTAL)
 			{
-				magnet.x = Math.max(MARGIN, MARGIN + Math.min((rank * WIDTH) + GUTTER*rank, MAXX-MARGIN));
-				magnet.y = Math.max(MARGIN, MARGIN + Math.min((cnt * HEIGHT) + GUTTER*cnt, MAXY-MARGIN));
+				magnet.x = Math.max(MARGIN, MARGIN + Math.min((file * WIDTH)  + GUTTER * file, MAXX - MARGIN));
+				magnet.y = CHEIGHT - HEIGHT - Math.max(MARGIN, MARGIN + Math.min((rank * HEIGHT) + GUTTER * rank, MAXY - MARGIN));
 			}
-			else
+			/*else
 			{
-				magnet.x = Math.max(MARGIN, MARGIN + Math.min((cnt * WIDTH) + GUTTER*cnt, MAXX-MARGIN));
+				magnet.x = Math.max(MARGIN, MARGIN + Math.min((cnt * WIDTH) + GUTTER*cnt/2, MAXX-MARGIN));
 				magnet.y = Math.max(MARGIN, MARGIN + Math.min((rank * HEIGHT) + GUTTER*rank, MAXY-MARGIN));
-			}
+			}*/
 
 			magnets.push(magnet);
 		}
-		moveAcc = magnets.length;
-		rank++;
 	}
+
+	moveAcc = accum;
+
 	debug('done setting up letters, total', accum);
 
 	socket.on('connect', skt =>
@@ -111,7 +138,7 @@ module.exports = (socket) =>
 		debug('user connected');
 		userCnt++;
 		//no need to keep track of individual presences (I think)
-		skt.emit('init', magnets, maxUserID++, userCnt, HORIZONTAL, moveAcc);
+		skt.emit('init', magnets, maxUserID++, userCnt, false, moveAcc);
 
 		socket.emit('user-enter', userCnt);
 
--- client/js/letters.js
+++ client/js/letters.js
@@ -22,6 +22,7 @@ const mkMagnet = (magnetDef) =>
 	el.style['z-index'] = magnetDef.zIndex;
 	el.className        = 'fridge-magnet';
 	el.onmousedown      = onMouseDown;
+	el.ontouchstart     = onTouchStart;
 
 	el.setAttribute('magnet-id', magnetDef.id);
 
@@ -47,6 +48,7 @@ const userEnter = (_userCnt) =>
 	document.getElementById('user-count').innerText = userCnt;
 	debug('user entered');
 }
+
 const userLeave = (_userCnt) =>
 {
 	userCnt = _userCnt;
@@ -56,9 +58,10 @@ const userLeave = (_userCnt) =>
 
 const init = (magnets, userID, _userCnt, horizontal, _moveAcc) =>
 {
+	debug('init');
 	document.getElementById('target').innerText = '';
 	magnets.forEach(mkMagnet);
-	myID    = userID;
+	myID    = userID || myID;
 	userCnt = _userCnt;
 	document.getElementById('user-count').innerText = userCnt;
 
@@ -68,49 +71,115 @@ const init = (magnets, userID, _userCnt, horizontal, _moveAcc) =>
 	moveAcc = _moveAcc;
 };
 
-const onMouseDown = function (e) 
+const onTouchStart = function (e)
 {
-	if (grabbedMag)
-		throw Error('Already dragging a magnet!');
+	let t = e.touches[0];
+	let po = document.getElementById('target').getBoundingClientRect();
+	let mo = e.target.getBoundingClientRect();
 
-	let id = this.getAttribute('magnet-id');
-	debug(`started dragging ${id}`);
+	let x = t.pageX-po.left;
+	let y = t.pageY-po.top;
 
-	grabbedMag            = this;
-	grabbedMag.onmouseup  = onMouseUp;
-	this.style.transition = 'none';
-	this.style['z-index'] = moveAcc+1;
-	initialOffset         = [e.offsetX, e.offsetY];
-
-	document.getElementById('target').onmousemove = onMouseMove;
-};
-
-const onMouseUp = function (e) 
+	return moveStart(e.target, x, y, t.pageX-mo.left, t.pageY-mo.top);
+}
+const onTouchMove = function (e)
 {
-	let id = this.getAttribute('magnet-id');
-
-	debug(`stopped dragging ${id}`);
-	grabbedMag.style.transition = null;
-
-	grabbedMag.onmouseup = undefined;
-	document.getElementById('target').onmousemove = undefined;
-	grabbedMag = undefined;
-
-	let px = getEmPixels();	
-
-	socket.emit('move', id, (e.clientX-initialOffset[0])/px, (e.clientY-initialOffset[1])/px, myID);
-};
-
+	let t = e.touches[0];
+	let po = document.getElementById('target').getBoundingClientRect();
+	
+	return move(e.target, t.pageX-po.left, t.pageY-po.top);
+}
+const onTouchEnd = function (e)
+{
+	let mo = grabbedMag.getBoundingClientRect();
+	let po = document.getElementById('target').getBoundingClientRect();
+	
+	return moveEnd(this, mo.left-po.left, mo.top-po.top);
+}
+const onMouseDown = function (e)
+{
+	return moveStart(this, e.clientX, e.clientY, e.offsetX, e.offsetY);
+}
 const onMouseMove = function (e)
 {
 	if (!e.buttons)
-		return onMouseUp.call(grabbedMag, e);
-	grabbedMag.style.left = (e.clientX-initialOffset[0]) + 'px';
-	grabbedMag.style.top  = (e.clientY-initialOffset[1]) + 'px';
+		return onMouseUp.call(this, e);
+
+	return move(this, e.offsetX, e.offsetY);
+}
+const onMouseUp = function (e)
+{
+	return moveEnd(this, e.offsetX, e.offsetY);
+}
+
+const moveStart = function (el, x, y, offsetX, offsetY) 
+{
+	debug('md');
+	if (grabbedMag)
+		throw Error('Already dragging a magnet!');
+
+	let id = el.getAttribute('magnet-id');
+	debug(`started dragging ${id}`);
+
+	grabbedMag                         = el;
+	grabbedMag.style.transition        = 'none';
+	grabbedMag.style['z-index']        = moveAcc+1;
+	
+	let mags = document.getElementsByClassName('fridge-magnet')
+	for(let i = 0; i < mags.length; i++)
+		mags[i].style['pointer-events'] = 'none';
+
+	initialOffset = [offsetX, offsetY];
+
+	let target = document.getElementById('target');
+
+	target.onmousemove   = onMouseMove;
+	target.onmouseup     = onMouseUp;
+	target.onmouseleave  = onMouseUp;
+	target.ontouchmove   = onTouchMove;
+	target.ontouchend    = onTouchEnd;
+	target.ontouchcancel = onTouchEnd;
+};
+
+const move = function (el, x, y)
+{
+	debug('mm');
+	
+	let px = getEmPixels();
+	
+	grabbedMag.style.left = ((x-initialOffset[0])/px) + 'rem';
+	grabbedMag.style.top  = ((y-initialOffset[1])/px) + 'rem';
+};
+
+const moveEnd = function (el, x, y)
+{
+	debug('mu');
+	let id = grabbedMag.getAttribute('magnet-id');
+
+	debug(`stopped dragging ${id}`);
+	grabbedMag.style.transition = null;
+	grabbedMag = undefined;
+
+	let mags = document.getElementsByClassName('fridge-magnet')
+	for(let i = 0; i < mags.length; i++)
+		mags[i].style['pointer-events'] = null;
+
+	let target = document.getElementById('target');
+
+	target.onmousemove   = undefined;
+	target.onmouseup     = undefined;
+	target.onmouseleave  = undefined;
+	target.ontouchmove   = undefined;
+	target.ontouchend    = undefined;
+	target.ontouchcancel = undefined;
+
+	let px = getEmPixels();
+	socket.emit('move', id, (x-initialOffset[0])/px, (y-initialOffset[1])/px, myID);
 };
 
 const connect = () =>
 {
+	debug('c');
 	socket.on('init', init);
 	socket.on('move', mvMagnet);
 	socket.on('user-enter', userEnter);
@@ -124,3 +193,18 @@ socket.on('connect', connect);
 
 global.socket = socket;
 
+const applyNarrowIfNarrowerThan = (aspect) => function (e)
+{
+	if (window.innerWidth / window.innerHeight < aspect)
+	{
+		document.body.className = 'narrow';
+		document.documentElement.className = 'narrow';
+	}
+	else
+	{
+		document.body.className = null;
+		document.documentElement.className = null;
+	}
+};
+
+window.onresize = window.onload = applyNarrowIfNarrowerThan(3/4);
